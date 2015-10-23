d06812e01d360b72960692f48e364248ae3e82af
Kevin Rockwood <kevin@rockwood.me>
2015-10-23 10:03:43 -0400

    Fix artists fixtures

--- app/mirage/fixtures/artists.js
+++ app/mirage/fixtures/artists.js
@@ -11,20 +11,20 @@ export default [
     name: "John Coltrane",
     bannerUrl: "/images/artists/john-coltrane-banner.jpg",
     thumbnailUrl: "/images/artists/john-coltrane-thumbnail.jpg",
-    albums: [4, 5]
+    albums: [4, 5, 6, 7]
   },
   {
     id: 3,
     name: "Ella Fitzgerald",
     bannerUrl: "/images/artists/ella-fitzgerald-banner.jpg",
     thumbnailUrl: "/images/artists/ella-fitzgerald-thumbnail.jpg",
-    albums: [6, 7, 8, 9]
+    albums: [8, 9, 10]
   },
   {
     id: 4,
     name: "Thelonious Monk",
     bannerUrl: "/images/artists/thelonious-monk-banner.jpg",
     thumbnailUrl: "/images/artists/thelonious-monk-thumbnail.jpg",
-    albums: [10, 11, 12]
+    albums: [11, 12]
   }
 ];
