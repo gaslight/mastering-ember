4088087068926ef7d365db07b92e2c165ed32b53
Kevin Rockwood <kevin@rockwood.me>
2015-10-27 13:55:47 -0400

    Add lab 2

---
+++ labs/lab_2.md
@@ -0,0 +1,7 @@
+# Converting to Ember Data
+
+Now, we'll convert our `artists` route to use Ember Data to fetch artists from the server. Run `git chechout lab-2`,
+and visit [localhost:4200/tests](http://localhost:4200/tests). You'll see that our acceptance test is failing because
+it's expecting to receive data from the server. Update the appropriate route to fetch the artists list from the server
+and make the tests pass.
+
--- tests/acceptance/viewing-the-artists-nav-test.js
+++ tests/acceptance/viewing-the-artists-nav-test.js
@@ -5,6 +5,7 @@ import startApp from 'mastering-ember/tests/helpers/start-app';
 module('Acceptance | Viewing the artists nav', {
   beforeEach: function() {
     this.application = startApp();
+    server.create('artist', { id: 1, name: "John Coltrane", thumbnailUrl: "/fixtures/test-thumbnail.jpg" });
   },
 
   afterEach: function() {
@@ -16,7 +17,7 @@ test('Viewing the artists list', function() {
   visit('/');
 
   andThen(function() {
-    assertElementExists("a.artist-link:contains('The Beatles')");
+    assertElementExists("a.artist-link:contains('John Coltrane')");
     assertElementExists("a.artist-link img[src='/fixtures/test-thumbnail.jpg']");
   });
 });
