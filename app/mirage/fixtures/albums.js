9ad6289f2ea463c291b0e77afa90a851f4a0f003
Kevin Rockwood <kevin@rockwood.me>
2015-10-26 20:33:04 -0400

    Move images to artist folders

--- app/mirage/fixtures/albums.js
+++ app/mirage/fixtures/albums.js
@@ -3,48 +3,48 @@ export default [
     id: 1,
     artist: 1,
     name: "Kind of Blue",
-    artworkUrl: "/images/albums/miles-davis-kind-of-blue.jpg",
+    artworkUrl: "/artists/miles-davis/kind-of-blue/miles-davis-kind-of-blue.jpg",
     tracks: [1, 2, 3, 4, 5]
   },
   {
     id: 2,
     artist: 1,
     name: "Sketches of Spain",
-    artworkUrl: "/images/albums/miles-davis-sketches-of-spain.jpg"
+    artworkUrl: "/artists/miles-davis/sketches-of-spain/miles-davis-sketches-of-spain.jpg"
   },
   {
     id: 3,
     artist: 1,
     name: "Birth of the Cool",
-    artworkUrl: "/images/albums/miles-davis-birth-of-the-cool.jpg"
+    artworkUrl: "/artists/miles-davis/birth-of-the-cool/miles-davis-birth-of-the-cool.jpg"
   },
 
   {
     id: 4,
     artist: 2,
     name: "Blue Trane",
-    artworkUrl: "/images/albums/john-coltrane-blue-trane.jpg",
+    artworkUrl: "/artists/john-coltrane/blue-train/john-coltrane-blue-trane.jpg",
     year: 1958
   },
   {
     id: 5,
     artist: 2,
     name: "Giant Steps",
-    artworkUrl: "/images/albums/john-coltrane-giant-steps.jpg",
+    artworkUrl: "/artists/john-coltrane/giant-steps/john-coltrane-giant-steps.jpg",
     year: 1959
   },
   {
     id: 6,
     artist: 2,
     name: "A Love Supreme",
-    artworkUrl: "/images/albums/john-coltrane-a-love-supreme.jpg",
+    artworkUrl: "/artists/john-coltrane/a-love-supreme/john-coltrane-a-love-supreme.jpg",
     year: 1965
   },
   {
     id: 7,
     artist: 2,
     name: "Coletrane's Sound",
-    artworkUrl: "/images/albums/john-coltrane-coltranes-sound.jpg",
+    artworkUrl: "/artists/john-coltrane/coltrane-sound/john-coltrane-coltranes-sound.jpg",
     year: 1964
   },
 
@@ -52,21 +52,21 @@ export default [
     id: 8,
     artist: 3,
     name: "Like Someone in Love",
-    artworkUrl: "/images/albums/ella-fitzgerald-like-someone-in-love.jpg",
+    artworkUrl: "/artists/ella-fitzgerald/like-someone-in-love/ella-fitzgerald-like-someone-in-love.jpg",
     year: 1957
   },
   {
     id: 9,
     artist: 3,
     name: "Lullabies of Birdland",
-    artworkUrl: "/images/albums/ella-fitzgerald-lullabies-of-birdland.jpg",
+    artworkUrl: "/artists/ella-fitzgerald/lullabies-of-birdland/ella-fitzgerald-lullabies-of-birdland.jpg",
     year: 1956
   },
   {
     id: 10,
     artist: 3,
     name: "Sweet and Hot",
-    artworkUrl: "/images/albums/ella-fitzgerald-sweet-and-hot.jpg",
+    artworkUrl: "/artists/ella-fitzgerald/sweet-and-hot/ella-fitzgerald-sweet-and-hot.jpg",
     year: 1955
   },
 
@@ -74,14 +74,14 @@ export default [
     id: 11,
     artist: 4,
     name: "Misterioso",
-    artworkUrl: "/images/albums/thelonious-monk-misterioso.jpg",
+    artworkUrl: "/artists/thelonious-monk/misterioso/thelonious-monk-misterioso.jpg",
     year: 1958
   },
   {
     id: 12,
     artist: 4,
     name: "Straight No Chaser",
-    artworkUrl: "/images/albums/thelonious-monk-straight-no-chaser.jpg",
+    artworkUrl: "/artists/thelonious-monk/straight-no-chaser/thelonious-monk-straight-no-chaser.jpg",
     year: 1967
   }
 ];
--- app/mirage/fixtures/artists.js
+++ app/mirage/fixtures/artists.js
@@ -2,29 +2,29 @@ export default [
   {
     id: 1,
     name: "Miles Davis",
-    bannerUrl: "/images/artists/miles-davis-banner.jpg",
-    thumbnailUrl: "/images/artists/miles-davis-thumbnail.jpg",
+    bannerUrl: "/artists/miles-davis/miles-davis-banner.jpg",
+    thumbnailUrl: "/artists/miles-davis/miles-davis-thumbnail.jpg",
     albums: [1, 2, 3]
   },
   {
     id: 2,
     name: "John Coltrane",
-    bannerUrl: "/images/artists/john-coltrane-banner.jpg",
-    thumbnailUrl: "/images/artists/john-coltrane-thumbnail.jpg",
+    bannerUrl: "/artists/john-coltrane/john-coltrane-banner.jpg",
+    thumbnailUrl: "/artists/john-coltrane/john-coltrane-thumbnail.jpg",
     albums: [4, 5, 6, 7]
   },
   {
     id: 3,
     name: "Ella Fitzgerald",
-    bannerUrl: "/images/artists/ella-fitzgerald-banner.jpg",
-    thumbnailUrl: "/images/artists/ella-fitzgerald-thumbnail.jpg",
+    bannerUrl: "/artists/ella-fitzgerald/ella-fitzgerald-banner.jpg",
+    thumbnailUrl: "/artists/ella-fitzgerald/ella-fitzgerald-thumbnail.jpg",
     albums: [8, 9, 10]
   },
   {
     id: 4,
     name: "Thelonious Monk",
-    bannerUrl: "/images/artists/thelonious-monk-banner.jpg",
-    thumbnailUrl: "/images/artists/thelonious-monk-thumbnail.jpg",
+    bannerUrl: "/artists/thelonious-monk/thelonious-monk-banner.jpg",
+    thumbnailUrl: "/artists/thelonious-monk/thelonious-monk-thumbnail.jpg",
     albums: [11, 12]
   }
 ];
